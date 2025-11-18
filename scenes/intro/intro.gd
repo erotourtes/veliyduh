fac4542f2f940f9f9360348751232044d6a0f77f
erotourtes <erotourtes@gmail.com>
2025-11-18 03:04:50 +0100

    feat: intro

--- scenes/intro/intro.gd
+++ scenes/intro/intro.gd
@@ -1,5 +1,12 @@
 extends Control
 
+func changeScene():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	changeScene()
+
+func _input(event: InputEvent) -> void:
+	if event.is_pressed():
+		changeScene()
+	
