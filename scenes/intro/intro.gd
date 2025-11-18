91620a0861188e6e49daf6448d77175ee0c737fe
erotourtes <erotourtes@gmail.com>
2025-11-18 22:37:09 +0100

    feat: use enter to skip

--- scenes/intro/intro.gd
+++ scenes/intro/intro.gd
@@ -7,6 +7,7 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	changeScene()
 
 func _input(event: InputEvent) -> void:
-	if event.is_pressed():
-		changeScene()
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ENTER or event.keycode == KEY_KP_ENTER:
+			changeScene()
 	
